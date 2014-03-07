ed211ef6fcda22b055cf25ea6d717c15de4a3d67
Anatoliy Ostroumov <ostroumov095@gmail.com>
2014-03-07 19:31:36 +0400

    unit test for sqlite data storage, and sqlite data storage works as expected

--- test/sequilizeModel.test.js
+++ test/sequilizeModel.test.js
@@ -3,24 +3,23 @@ var hunt = require('./../index.js'),
   should = require('should'),
   async = require('async'),
   Hunt;
-/*/
+//*/
 describe('Sequilize test for SQLite database model', function () {
   before(function (done) {
     Hunt = hunt({
-      'sequelizeUrl':'sqlite://:memory:'
+      'sequelizeUrl':'sqlite://localhost/'
     });
 
     Hunt.extendModel('Planet', function(core){
-
       return core.sequelize.define('Planet', {
-        name: core.Sequelize.STRING,
+        name: core.Sequelize.STRING
       });
     });
 
     Hunt.on('start', function () {
       Hunt.sequelize.sync().success(function() {
         Hunt.model.Planet.create({
-          username: 'Earth'
+          name: 'Earth'
         }).success(function(planet) {
           done(null);
         });
@@ -30,7 +29,21 @@ describe('Sequilize test for SQLite database model', function () {
   });
 
   it('populates model with object, that looks like sequilize model', function(){
-    Hunt.model.Planet.should.be.equal(1);
+    Hunt.model.Planet.name.should.be.equal('Planet');
+    Hunt.model.Planet.tableName.should.be.equal('Planets');
+  });
+
+  it('allows us to find data from it', function(done){
+    Hunt.model.Planet
+      .find({ where: {name: 'Earth'} })
+      .success(function(planet) {
+        if(planet){
+          planet.name.should.be.equal('Earth');
+          done(null);
+        } else {
+          done(new Error('Error finding planet?'));
+        }
+      });
   });
 
   after(function (done) {
