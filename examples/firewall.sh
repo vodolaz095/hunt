211b9756707fc40c4ed38e72728f4b0dcf42f73c
Anatoliy Ostroumov <ostroumov095@gmail.com>
2014-03-08 22:39:29 +0400

    firewall setup script

---
+++ examples/firewall.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Official script to set up firewall properly with Hunt.js
+#(c) Ostroumov Anatolij
+# https://github.com/vodolaz095/hunt
+
+
+#drop all rules
+iptables -F
+
+#basic anti ddos
+iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
+
+#to access localhost
+iptables -A INPUT -i lo -j ACCEPT
+
+#open for ssh and http(s) servers
+iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+
+#allow sending anything
+iptables -P OUTPUT ACCEPT
+
+#do no accept other incoming transmissions
+iptables -P INPUT DROP
