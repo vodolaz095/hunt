0bfdf153183b4761f3285f360617b27202a7e3ca
Anatoliy Ostroumov <ostroumov095@gmail.com>
2014-04-18 23:33:39 +0400

    request is now development only module

--- examples/lib/pinger.js
+++ examples/lib/pinger.js
@@ -1,4 +1,4 @@
-var request = require('request'),
+var request = require('http').request,
   url = require('url');
 
 //socket.io listener to issue ping requests
@@ -8,7 +8,7 @@ function isUrl(payload){
   if(typeof payload === 'string'){
     var parameters = url.parse(payload);
       if(parameters.protocol === 'http:' || parameters.protocol === 'https:' ){
-        return payload;
+        return parameters;
       } else {
         return false;
       }
@@ -20,14 +20,14 @@ function isUrl(payload){
 module.exports = exports = function(payload, socket){
   var a = isUrl(payload);
   if(a){
-    socket.emit('pingerAnswer', 'Trying to ping '+a+'...');
-    request.head(a, function(error, response, body){
-      if(error){
-        socket.emit('pingerAnswer','Error! '+error.toString());
-      } else {
-        socket.emit('pingerAnswer', a+' - statusCode '+response.statusCode);
-      }
+    socket.emit('pingerAnswer', 'Trying to ping '+a.href+'...');
+    var req = request({'method':'HEAD', 'hostname': a.hostname, 'port':a.port }, function(response){
+      socket.emit('pingerAnswer', a.href+' - statusCode '+response.statusCode);
     });
+    req.on('error', function(error){
+      socket.emit('pingerAnswer','Error! '+error.toString());
+    });
+    req.end();
   } else {
     socket.emit('pingerAnswer','Error! Unable to parse URL!');
   }
