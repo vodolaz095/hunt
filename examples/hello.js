74aab2013a3db32865eafccec7d3c6601f432641
Ostroumov Anatolij <ostroumov095@gmail.com>
2015-06-13 23:44:46 +0300

    fixing

--- examples/hello.js
+++ examples/hello.js
@@ -1,9 +1,9 @@
-var hunt = require('hunt')({
+require('./../index.js')({ //it have to be `require('hunt')`
   'port': 3000
 })
-  .extendController('/', function (core, router) {
-    router.get('/', function (req, res) {
-      res.send('Hello, world!');
-    });
-  })
-  .startWebServer();
+.extendController('/', function (core, router) {
+  router.get('/', function (req, res) {
+    res.send('Hello, world!');
+  });
+})
+.startWebServer();
