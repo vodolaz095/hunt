9d75bae36d75bc9f154ea579575c7b9b4fbacea2
Ostroumov Anatolij <ostroumov095@gmail.com>
2015-09-11 20:21:00 +0300

    Improve continious integration features on heroku

--- postinstall.sh
+++ postinstall.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 
-if [ "$hostUrl"="https://huntjs.herokuapp.com/" ] || [ "$LOGNAME"="vodolaz095" ]; then
-# Generate documentation
+if [ "$hostUrl"="https://huntjs.herokuapp.com/" ] || [ -f herokurules.txt ]; then
+# Clear old files
   rm -rf examples/public/documentation/
   rm -rf tutorials/Changelog.md -rf
-  cp CHANGELOG.md tutorials/Changelog.md
+  rm -rf examples/public/coverage
   rm -rf tutorials/ServerConfig.md
-  cp examples/serverConfigsExamples/README.md tutorials/ServerConfig.md
 
+# Generate coverage report
+  node node_modules/.bin/istanbul cover ./node_modules/.bin/_mocha
+
+# Generate test report
+  node node_modules/.bin/mocha -G --reporter markdown > tutorials/test.md
+
+# Generate documentation
+  cp CHANGELOG.md tutorials/Changelog.md
+  cp examples/serverConfigsExamples/README.md tutorials/ServerConfig.md
+  cp CHANGELOG.md tutorials/Changelog.md
+  cp examples/serverConfigsExamples/README.md tutorials/ServerConfig.md
   node node_modules/jsdoc/jsdoc.js -c jsdoc.conf.json --pedantic -u tutorials/
   echo "Documentation generated in examples/public/documentation"
 
-# Generate coverage report
-  rm -rf examples/public/coverage
-  node node_modules/.bin/istanbul cover ./node_modules/.bin/_mocha
 else
   echo "HuntJS installing ok!"
 fi
