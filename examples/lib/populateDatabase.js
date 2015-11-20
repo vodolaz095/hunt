2a73f67d53207c4a8ac7b41c5f5fd930168dd863
Ostroumov Anatolij <ostroumov095@gmail.com>
2015-11-20 15:40:54 +0300

    fix populate database

--- examples/lib/populateDatabase.js
+++ examples/lib/populateDatabase.js
@@ -2,44 +2,38 @@
 
 var
   winston = require('winston'),
-  preys = [
-  {
-    '_id': '557caf8ee7c36e8011b92ea7',
-    'name': 'Alan Schaefer',
-    'scored': false,
-    'priority': 10
-  },
-  {
-    '_id': '557caf8ee7c36e8011b92ea8',
-    'name': 'George Dillon',
-    'scored': true,
-    'priority': 9
-  },
-  {
-    '_id': '557caf8ee7c36e8011b92ea9',
-    'name': 'Rick Hawkins',
-    'scored': true,
-    'priority': 8
-  },
-  {
-    '_id': '557caf8ee7c36e8011b92eaa',
-    'name': 'Blain Cooper',
-    'scored': true,
-    'priority': 7
-  },
-  {
-    '_id': '557caf8ee7c36e8011b92eab',
-    'name': 'Billy Sole',
-    'scored': true,
-    'priority': 6
-  },
-  {
-    '_id': '557caf8ee7c36e8011b92eac',
-    'name': 'Anna Goncalves',
-    'scored': false,
-    'priority': 0
-  }
-];
+  preys = {
+    '557caf8ee7c36e8011b92ea7': {
+      'name': 'Alan Schaefer',
+      'scored': false,
+      'priority': 10
+    },
+    '557caf8ee7c36e8011b92ea8': {
+      'name': 'George Dillon',
+      'scored': true,
+      'priority': 9
+    },
+    '557caf8ee7c36e8011b92ea9': {
+      'name': 'Rick Hawkins',
+      'scored': true,
+      'priority': 8
+    },
+    '557caf8ee7c36e8011b92eaa': {
+      'name': 'Blain Cooper',
+      'scored': true,
+      'priority': 7
+    },
+    '557caf8ee7c36e8011b92eab': {
+      'name': 'Billy Sole',
+      'scored': true,
+      'priority': 6
+    },
+    '557caf8ee7c36e8011b92eac': {
+      'name': 'Anna Goncalves',
+      'scored': false,
+      'priority': 0
+    }
+  };
 
 //populating the trophies' collection in database
 module.exports = exports = function (hunt) {
@@ -47,7 +41,6 @@ module.exports = exports = function (hunt) {
   hunt.async.series([
     function (cb) {
       hunt.model.User.findOneAndUpdate({'_id': '55b0c81ee523c6a60c4325ad'}, {
-        '_id': '55b0c81ee523c6a60c4325ad',
         'displayName': 'Gamekeeper',
         'root': false,
         'accountVerified': true,
@@ -65,9 +58,10 @@ module.exports = exports = function (hunt) {
       });
     },
     function (cb) {
-      hunt.async.map(preys, function (prey, clb) {
-        prey.author = gameMasterId;
-        hunt.model.Trophy.findOneAndUpdate({'_id': prey._id}, prey, {'upsert': true}, function (error, trophyUpdated) {
+      hunt.async.map(Object.keys(preys), function (key, clb) {
+        var p = preys[key];
+        p.author = gameMasterId;
+        hunt.model.Trophy.findOneAndUpdate({'_id': key}, p, {'upsert': true}, function (error, trophyUpdated) {
           if (error) {
             clb(error);
           } else {
